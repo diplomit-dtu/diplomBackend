8b844caf54d29c209985f237d82935ff65d77aa6
cbudtz <chrbudtz@gmail.com>
2017-04-28 14:18:11 +0200

    Hello Service

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,9 @@
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/** Initial Configuration
+ * Created by Christian on 28-04-2017.
+ */
+@ApplicationPath("/rest")
+public class AppConfig extends Application{
+}
---
+++ src/main/java/HelloService.java
@@ -0,0 +1,13 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/** Testing Service
+ * Created by Christian on 28-04-2017.
+ */
+@Path("hello")
+public class HelloService {
+    @GET
+    public String getHello(){
+        return "HELLO, REST";
+    }
+}
