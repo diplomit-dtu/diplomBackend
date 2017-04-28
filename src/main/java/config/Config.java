d3686914f6a8d06c711b2337fea0a47196d37b6e
cbudtz <chrbudtz@gmail.com>
2017-04-28 20:19:10 +0200

    Campusnet login implementation

---
+++ src/main/java/config/Config.java
@@ -0,0 +1,8 @@
+package config;
+
+/** Global configuration
+ * Created by Christian on 28-04-2017.
+ */
+public class Config {
+    public static final String CAMPUSNET_LOGIN_URL = "https://auth.dtu.dk/dtu/login";
+}
--- src/main/java/rest/CampusNetService.java
+++ src/main/java/rest/CampusNetService.java
@@ -1,18 +1,29 @@
 package rest;
 
+import config.Config;
+import deployment.DeployConfig;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.net.URI;
 
 /** Service for Campusnet redirects
  * Created by Christian on 28-04-2017.
  */
 @Path("cn")
 public class CampusNetService {
+    @Path("login")
+    @GET
+    public Response getLogin(){
+        URI uri = URI.create(Config.CAMPUSNET_LOGIN_URL + "?service=" + DeployConfig.CN_REDIRECT_URL);
+        return Response.temporaryRedirect(uri).build();
+    }
 
     @GET
     public Response getRedirect(@QueryParam("ticket") String ticket){
+
         return Response.ok().entity("ticket:" + ticket).build();
     }
 }
