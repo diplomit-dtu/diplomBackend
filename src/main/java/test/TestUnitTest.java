409ce6a5727a8492024aa24bf8eb6141c6a5039c
cbudtz <chrbudtz@gmail.com>
2017-05-02 15:09:17 +0200

    fail Test for Travis

---
+++ src/main/java/test/TestUnitTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/** Test Unit Test
+ * Created by Christian on 02-05-2017.
+ */
+public class TestUnitTest {
+
+    @Test
+    public void test(){
+        assertTrue(true);
+        fail();
+    }
+}
