164d98f661cc6d1ae93d2af1de95b60cc3089de2
cbudtz <chrbudtz@gmail.com>
2017-05-16 16:43:16 +0200

    Fixing CORS-problem

--- src/main/java/rest/JWTFilter.java
+++ src/main/java/rest/JWTFilter.java
@@ -2,6 +2,7 @@ package rest;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
@@ -20,15 +21,20 @@ public class JWTFilter implements Filter {
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if (!(request instanceof HttpServletRequest)) {
+        if (!(request instanceof HttpServletRequest) || !(response instanceof HttpServletResponse)) {
             throw new WebApplicationException("Something went wrong while filtering request");
         } else {
             HttpServletRequest httpRequest = ((HttpServletRequest) request);
+            HttpServletResponse httpResponse = ((HttpServletResponse) response);
+
             String url = httpRequest.getRequestURI();
             String params = httpRequest.getQueryString();
             String authHeader = httpRequest.getHeader("Authorization");
             System.out.println("Caught in filter - Url: " + url + " Query: " + params + "Headers: " + authHeader);
-
+            httpResponse.addHeader("Access-Control-Allow-Origin", "*");
+            httpResponse.addHeader("Access-Control-Allow-Origin", "*");
+            httpResponse.addHeader("Access-Control-Allow-Credentials:", "true");
+            httpResponse.addHeader("Access-Control-Expose-Headers","Authorization");
             chain.doFilter(request,response);
         }
     }
