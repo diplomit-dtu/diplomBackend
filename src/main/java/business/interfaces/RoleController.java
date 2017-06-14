b0004dae74b7850838eee9b08f093abfb9eb8c45
cbudtz <chrbudtz@gmail.com>
2017-06-14 20:22:37 +0200

    committing missing interface

---
+++ src/main/java/business/interfaces/RoleController.java
@@ -0,0 +1,11 @@
+package business.interfaces;
+
+import data.dbDTO.Role;
+
+/**
+ * Created by Christian on 08-06-2017.
+ */
+public interface RoleController {
+    Role getRole(String roleId);
+
+}
