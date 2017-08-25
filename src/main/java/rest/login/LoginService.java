27f64a15d5e696d5c3fcfd497587fcec7e276c3f
cbudtz <chrbudtz@gmail.com>
2017-08-25 15:07:11 +0200

    Added robustness for courseplan parsing Guest login at /rest/guestlogin

--- src/main/java/rest/login/LoginService.java
+++ src/main/java/rest/login/LoginService.java
@@ -11,9 +11,6 @@ import util.FileLoader;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.HashMap;
@@ -22,7 +19,7 @@ import java.util.Map;
 /** StubService for later implementation
  * Created by Christian on 29-04-2017.
  */
-@Path("guestLogin")
+@Path("guestlogin")
 public class LoginService {
 
     @GET
