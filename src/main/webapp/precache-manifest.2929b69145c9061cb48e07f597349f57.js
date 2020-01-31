d63b1b339d17a7f362f12b7242eba217c0ff0119
chbu <chbu@dtu.dk>
2020-01-31 10:09:20 +0100

    Updating react scripts and cleaning up

---
+++ src/main/webapp/precache-manifest.2929b69145c9061cb48e07f597349f57.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558abcacd45a3164437739d4f7412b2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1dec63215b7cd8e39d4",
+    "url": "/static/css/2.cb0b67f6.chunk.css"
+  },
+  {
+    "revision": "abf8e3d403a0c753a222",
+    "url": "/static/css/main.3a1be71a.chunk.css"
+  },
+  {
+    "revision": "d1dec63215b7cd8e39d4",
+    "url": "/static/js/2.d895f0bf.chunk.js"
+  },
+  {
+    "revision": "317089322821504487d854025ef7a225",
+    "url": "/static/js/2.d895f0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf8e3d403a0c753a222",
+    "url": "/static/js/main.ece4578c.chunk.js"
+  },
+  {
+    "revision": "15a5a5eda15a538d155d",
+    "url": "/static/js/runtime-main.ae0c85bc.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "ddb71c788f78fc1c1d608988bbbc9c7e",
+    "url": "/static/media/ripple.ddb71c78.svg"
+  }
+]);
